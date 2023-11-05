c3428f71c0507d5e0a693e7f76e711600c61dd56
Levi Heßmann <levihsmn@gmail.com>
2023-11-05 01:32:33 +0100

    prettier code

--- src/components/Alternative.tsx
+++ src/components/Alternative.tsx
@@ -49,12 +49,18 @@ export default class Alternative extends React.Component<AlternativeProps> {
           yearly.classList.add("text-xs", "text-gray-400", "ml-1");
           price.classList.add("text-right");
 
-          priceText.classList.add('cursor-help');
+          priceText.classList.add("cursor-help");
 
           priceText.setAttribute("data-tooltip-id", "my-tooltip");
-          priceText.setAttribute("data-tooltip-content", "Renewal: " + item.priceRenew + "€ · Transfer: " + item.priceTransfer + "€");
+          priceText.setAttribute(
+            "data-tooltip-content",
+            "Renewal: " +
+              item.priceRenew +
+              "€ · Transfer: " +
+              item.priceTransfer +
+              "€",
+          );
           priceText.setAttribute("data-tooltip-place", "right");
-          
 
           const provider = document.createElement("p");
           provider.classList.add("text-xs", "text-gray-400");
@@ -95,14 +101,20 @@ export default class Alternative extends React.Component<AlternativeProps> {
             .then((json) => {
               if (json.useable) {
                 free.setAttribute("data-tooltip-id", "my-tooltip");
-                free.setAttribute("data-tooltip-content", "The domain is free to register.");
+                free.setAttribute(
+                  "data-tooltip-content",
+                  "The domain is free to register.",
+                );
                 free.setAttribute("data-tooltip-place", "right");
 
                 free.innerHTML =
                   '<svg xmlns="http://www.w3.org/2000/svg" height="1em" class="fill-green-600" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>';
               } else {
                 free.setAttribute("data-tooltip-id", "my-tooltip");
-                free.setAttribute("data-tooltip-content", "The domain is not free.");
+                free.setAttribute(
+                  "data-tooltip-content",
+                  "The domain is not free.",
+                );
                 free.setAttribute("data-tooltip-place", "right");
 
                 free.innerHTML =
